06573e474330cab4aaa7e5887bcf00770616785e
chenriqueweb <carlos.henrique.mobile@gmail.com>
2023-08-18 17:31:40 -0300

    Atualizações

--- src/main/java/br/com/via/consumer/config/KafkaConfig.java
+++ src/main/java/br/com/via/consumer/config/KafkaConfig.java
@@ -9,7 +9,7 @@ public class KafkaConfig {
     public static Properties getProperties() throws IOException {
         Properties props = new Properties();
 
-        FileInputStream file = new FileInputStream("src/main/resources/properties");
+        FileInputStream file = new FileInputStream("src/main/resources/application.properties");
 
         props.load(file);
 
